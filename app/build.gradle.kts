b8f248825797b2f4bd88edceb211e47ec59ac624
Israel <isra.vera.14.13@gmail.com>
2026-09-17 14:46:21 -0300

    Connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -32,10 +35,6 @@ android {
 
     buildFeatures{
         compose = true
-    }
-
-    composeOptions{
-        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
